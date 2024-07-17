f5acbb247537e592df35530e463dacfbee4912f4
Shegzboyer12 <segunjoshua85@gmail.com>
2024-07-17 14:24:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Counter from './components/Counter';
+import UserProfile from './components/UserProfile';
 
 function App() {
+  const [name, setName] = useState('John Doe');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="My Sample App" />
+      <UserProfile name={name} />
+      <Counter />
     </div>
   );
 }
